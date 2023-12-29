78a41954e5ad59814ba9872b0c4615f098b02f3b
Cherry Mg Mg <cherry@kali.Min>
2023-12-29 07:05:32 -0500

    first Initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import { useMemo } from "react";
+import styled from "styled-components";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Expenses from "./Components/Expenses/Expenses";
+import Incomes from "./Components/Incomes/Incomes";
+import Navigation from "./Components/Navigation/Navigation";
+import Orb from "./Components/Orb/Orb";
+import { useGlobalContext } from "./context/globalContext";
+import bg from './img/bg.png'
+import { MainLayout } from "./styles/Layouts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+   
+   const [active,setActive]=useState(1)
+
+
+   const global=useGlobalContext();
+   console.log(global)
+
+
+  
+ 
+     const orbMemo=useMemo(()=>{
+
+        return <Orb/>
+
+    },[])
+
+
+
+
+
+    const displayData=()=>{
+      switch(active){
+         case 1:
+            return <Dashboard/>
+         case 2:
+            return <Dashboard/>
+         case 3:
+            return <Incomes/>
+         case 4:
+            return <Expenses/>
+         default :
+            return <Dashboard/>
+      }
+    }
+
+    
+    return(
+    
+    <AppStyled bg={bg} className="App">
+
+     {orbMemo}
+
+     <MainLayout>
+
+        <Navigation active={active} setActive={setActive}/>
+
+        <main>
+       {displayData()}
+        </main>
+      
+     </MainLayout>
+   
+
+    </AppStyled>
+    )
 }
+
+const AppStyled=styled.div`
+height:100vh;
+background-image:url(${props=>props.bg});
+position:relative;
+main{
+   flex:1;
+   background:rgba(252,246,249,0.78);
+   border:3px solid #FFFFFF;
+   backdrop-filter:blur(4.5px);
+   border-radius:32px;
+   overflow-x:hidden;
+   &::-webkit-scrollbar{
+      width:0;
+   }
+}
+
+
+
+`;
+
+
 
 export default App;
